e9aa326ee0f1fc7d22b34dd4bd488cb94d9e9da0
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-10-31 04:08:34 +0000

    002. Introduction Constractor Functions.mp4

--- src/index.mjs
+++ src/index.mjs
@@ -1,120 +1,35 @@
-import "./styles.css";
+//Example Class
+class Person {
+  //properti
+  name = "Rasyid";
 
-const startGameBtn = document.getElementById("start-game-btn");
-
-const ROCK = "ROCK";
-const PAPER = "PAPER";
-const SCISSORS = "SCISSORS";
-const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW = "DRAW";
-const RESULT_PLAYER_WINS = "PLAYER_WINS";
-const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
-
-let gameIsRunning = false;
-
-const getPlayerChoice = function () {
-  const selection = prompt(
-    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
-    ""
-  ).toUpperCase();
-
-  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return;
+  //this is property for auto run when instantiate
+  constructor() {
+    this.age = 24;
   }
 
-  return selection;
-};
-
-const getComputerChoice = function () {
-  const randomValue = Math.random();
-  if (randomValue < 0.34) {
-    return ROCK;
-  } else if (randomValue < 0.67) {
-    return PAPER;
-  } else {
-    return SCISSORS;
+  //method
+  greet() {
+    console.log(
+      "Hi, I am " + this.name + " and I am " + this.age + " years old."
+    );
   }
-};
+}
 
-// this is using arrow function
-const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => {
-  if (cChoice === pChoice) {
-    return RESULT_DRAW;
-  } else if (
-    (cChoice === ROCK && pChoice === PAPER) ||
-    (cChoice === PAPER && pChoice === SCISSORS) ||
-    (cChoice === SCISSORS && pChoice === ROCK)
-  ) {
-    return RESULT_PLAYER_WINS;
-  } else {
-    return RESULT_COMPUTER_WINS;
-  }
-};
+//this is intantiate Class
+const person = new Person();
+person.greet();
 
-//this is using anounymous function
-startGameBtn.addEventListener("click", function () {
-  if (gameIsRunning) {
-    return;
-  }
-  gameIsRunning = true;
-  console.log("Game is starting...");
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
+//Example Constructor Function
+function PersonConstructor() {
+  this.age = 24;
+  this.name = "Rasyid";
+  this.greet = function () {
+    console.log(
+      "Hi, I am " + this.name + " and I am " + this.age + " years old."
+    );
+  };
+}
 
-  let winner;
-
-  if (playerChoice) {
-    winner = getWinner(computerChoice, playerChoice);
-  } else {
-    winner = getWinner(computerChoice);
-  }
-
-  let message = `You had picked ${
-    playerChoice || DEFAULT_USER_CHOICE
-  }, computer picked ${computerChoice}, therefore you `;
-
-  if (winner === RESULT_DRAW) {
-    message = message + "had a draw";
-  } else if (winner == RESULT_PLAYER_WINS) {
-    message = message + "won.";
-  } else {
-    message = message + "lost.";
-  }
-
-  alert(message);
-  gameIsRunning = false;
-});
-
-//rest arguments and callback function
-const combine = (resultHandler, operator, ...numbers) => {
-  const validateNumber = (number) => (isNaN(number) ? 0 : number);
-
-  let sum = 0;
-  for (const num of numbers) {
-    if (operator === "ADD") {
-      sum += validateNumber(num);
-    } else {
-      sum -= validateNumber(num);
-    }
-  }
-
-  //this is call callback
-  resultHandler(sum);
-};
-
-const showResult = (messageText, result) => {
-  alert(messageText + " " + result);
-};
-
-// this is using bind for delay and run first in this and then in callback
-combine(
-  showResult.bind(null, "the result after adding all number is:"),
-  "ADD",
-  1,
-  2,
-  4,
-  9,
-  10,
-  22
-);
+const personConstructor = new PersonConstructor();
+personConstructor.greet();
