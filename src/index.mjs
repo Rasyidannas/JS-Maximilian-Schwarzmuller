613981e8034d971bffe75c37c958bd8c0102c2cd
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-11-27 10:47:54 +0000

    12/3. Creating Project List & Parsing Element Data.mp4

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,19 @@ class Tooltip {}
 
 class ProjectItem {}
 
-class ProjectList {}
+class ProjectList {
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    console.log(prjItems);
+  }
+}
 
-class App {}
+class App {
+  //this static can call without intatiate
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+  }
+}
+
+App.init();
