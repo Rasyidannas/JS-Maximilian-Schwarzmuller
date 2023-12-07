24ed436e15968e65a5e60adf36dfdd62d0b2ed91
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-12-07 03:47:11 +0000

    12/5. Passing Method References Around.mp4

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,9 @@ class Tooltip {}
 
 // this is for box item
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id; //this is property
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton(); //this method will auto run when instantiate
     this.connectSwitchButton();
   }
@@ -14,7 +15,7 @@ class ProjectItem {
   connectSwitchButton() {
     const projectItemElement = document.getElementById(this.id);
     const switchBtn = projectItemElement.querySelector("button:last-of-type");
-    switchBtn.addEventListener("click");
+    switchBtn.addEventListener("click", this.updateProjectListsHandler);
   }
 }
 
@@ -24,23 +25,35 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type; //this is store as property
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     // console.log(prjItems);
     for (const prjItem of prjItems) {
       //prjItems berisi nodeList
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
-    console.log(this.projects);
+    // console.log(this.projects);
   }
 
-  addProject() {}
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction; //this will store as property and get element for change active and finish
+  }
+
+  addProject() {
+    console.log(this);
+  }
 
   //this will move active to finish
   switchProject(projectId) {
     // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
     // this.projects.splice(projectIndex, 1);
 
-    //this is same way like above
+    //this for get element for will change finish and active
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+
+    //this is same way like above for remove projects element
     this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
@@ -50,6 +63,13 @@ class App {
   static init() {
     const activeProjectList = new ProjectList("active");
     const finishedProjectList = new ProjectList("finished");
+
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    ); //this is callback and store element will change active to finish to addProject
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    ); //this is callback and store element will change active to finish to addProject
   }
 }
 
