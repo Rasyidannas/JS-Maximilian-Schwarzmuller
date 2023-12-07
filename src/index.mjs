394783d8054b4aff6abd40a88459a38c40c1c7b8
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2023-12-07 03:12:56 +0000

    12/4. Starting with Switch Project Logic.mp4

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,47 @@
 class Tooltip {}
 
-class ProjectItem {}
+// this is for box item
+class ProjectItem {
+  constructor(id) {
+    this.id = id; //this is property
+    this.connectMoreInfoButton(); //this method will auto run when instantiate
+    this.connectSwitchButton();
+  }
 
+  connectMoreInfoButton() {}
+
+  //this for change button finish to active
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
+  }
+}
+
+//this is for box project (active and finish box)
 class ProjectList {
+  //this is field
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    // console.log(prjItems);
+    for (const prjItem of prjItems) {
+      //prjItems berisi nodeList
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  //this will move active to finish
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+
+    //this is same way like above
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
