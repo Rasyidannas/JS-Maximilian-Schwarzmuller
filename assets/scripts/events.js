059a0447958e02c046f12047103f50821cc3434a
rasyid annas <rasshit.dsgn@gmail.com>
2023-12-19 18:14:57 +0700

    14/311. Working with preventDefault().mp4

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -25,3 +25,11 @@ buttons.forEach((btn) => {
 window.addEventListener("scroll", (event) => {
   console.log(event);
 });
+
+//this is for form
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
