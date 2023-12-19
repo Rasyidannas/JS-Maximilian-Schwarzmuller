2e8c71e73ccf621b8d124c25528bc218c3e778fa
rasyid annas <rasshit.dsgn@gmail.com>
2023-12-19 15:21:42 +0700

    14/307. Removing event Listenters.mp4

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,4 +4,15 @@ const buttonClickHandler = () => {
   alert("Button was clicked!");
 };
 
-button.onclick = buttonClickHandler;
+const anotherButtonClickHandler = () => {
+  console.log("THis was clicked!");
+};
+
+// button.onclick = buttonClickHandler;
+
+button.addEventListener("click", buttonClickHandler);
+
+//this will remove above addEventListener. Only work with function expression for second argument and don't bind function
+setTimeout(() => {
+  button.removeEventListener("click", buttonClickHandler);
+}, 2000);
