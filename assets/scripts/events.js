b07cae048148a8cdd65aa361bc3a36c8298c4d32
rasyid annas <rasshit.dsgn@gmail.com>
2023-12-23 21:06:21 +0700

    14/313. Event Propagation & *stopPropagation()

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll("button");
+const button = document.querySelector("button");
 
 const buttonClickHandler = (event) => {
   // event.target.disabled = true;
@@ -18,18 +18,35 @@ const anotherButtonClickHandler = () => {
 //   button.removeEventListener("click", buttonClickHandler);
 // }, 2000);
 
-buttons.forEach((btn) => {
-  btn.addEventListener("mouseenter", buttonClickHandler);
-});
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", buttonClickHandler);
+// });
 
-window.addEventListener("scroll", (event) => {
-  console.log(event);
-});
+// window.addEventListener("scroll", (event) => {
+//   console.log(event);
+// });
 
 //this is for form
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector("div");
+
+div.addEventListener(
+  "mouseenter",
+  (event) => {
+    console.log("CLICKED DIV");
+    console.log(event);
+  }
+  //true //this will run before/first tahn addEventListener in child element (addEventListener in bellow) or capturing
+);
+
+button.addEventListener("mouseenter", (event) => {
+  event.stopPropagation(); //this will stop other addEventListener in parent element
+  console.log("CLICKED BUTTON");
   console.log(event);
 });
