0ce6104043be2760bef70c4fcd3fc4cdbad4b518
rasyid annas <rasshit.dsgn@gmail.com>
2023-12-27 05:12:58 +0700

    16/343. Tagged Templates.mp4

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(5, 10));
+
+//////// TEMPLATE STRINGS ///////
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  return `This is a product!`;
+}
+
+const prodName = "JavaScript Course";
+const prodPrice = 29.99;
+
+//call above function using tempate strings and will save in first argument as array
+const productOutput = productDescription`This product (${prodName} is ${prodPrice})`;
+console.log(productOutput);
