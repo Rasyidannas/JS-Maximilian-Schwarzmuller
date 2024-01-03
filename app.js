8b286fcf58c33330beb08425861aa7c70c34aac4
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-04 06:29:23 +0700

    17/352. Sync + Async Code - The Execution Order.mp4

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const button = document.querySelector("button");
 const output = document.querySelector("p");
 
 function trackUserHandler() {
-  console.log("Clicked!");
+  navigator.geolocation.getCurrentPosition(
+    (posData) => console.log(posData),
+    (error) => console.log(error)
+  );
+
+  console.log("Getting position...");
 }
 
 button.addEventListener("click", trackUserHandler);
