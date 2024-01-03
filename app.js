f1268d9605a0d28e5a5784b1db54dc8973dfc59f
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-03 23:15:17 +0700

    17/350. Understanding Asynchronous Code Execution(Async Code).mp4

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-function randomIntBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+function trackUserHandler() {
+  console.log("Clicked!");
 }
 
-console.log(randomIntBetween(5, 10));
-
-//////// TEMPLATE STRINGS ///////
-function productDescription(strings, productName, productPrice) {
-  console.log(strings);
-  console.log(productName);
-  console.log(productPrice);
-  return `This is a product!`;
-}
-
-const prodName = "JavaScript Course";
-const prodPrice = 29.99;
-
-//call above function using tempate strings and will save in first argument as array
-const productOutput = productDescription`This product (${prodName} is ${prodPrice})`;
-console.log(productOutput);
+button.addEventListener("click", trackUserHandler);
