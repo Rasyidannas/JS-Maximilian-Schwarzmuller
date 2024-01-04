bc83cca76961fbef512338b6d28073808a98c76d
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-04 21:19:56 +0700

    17/354. Getting Started with Promise.mp4

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout(() => console.log(posData), 2000);
+      setTimer(2000).then((data) => console.log(data, posData));
+      // console.log(posData);
     },
     (error) => console.log(error)
   );
 
-  setTimeout(() => console.log("Timer done!"), 0);
+  setTimer(1000).then((data) => console.log("Timer done!"));
 
   console.log("Getting position...");
 }
