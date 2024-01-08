b5ff92f34d6a6dc79256dc92f51a2464106ab3a8
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-09 01:55:55 +0700

    18/370. JSON Data & Parsing Data.mp4

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,22 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+
+xhr.responseType = "json"; //this will auto parse & stringify json
+
+xhr.onload = function () {
+  //   console.log(xhr.response);
+  const listOfPosts = xhr.response;
+  //   console.log(listOfPosts);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true); //this is for cloning/copy postTemplate
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 
 xhr.send();
