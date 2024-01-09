7eddc7c8f43a3718d1fde6429c90db289b4b23e7
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-10 04:49:47 +0700

    18/372. Promisifying Http Requests (with XMLHttpRequest).mp4

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,40 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.open(method, url);
 
-xhr.responseType = "json"; //this will auto parse & stringify json
+    xhr.responseType = "json"; //this will auto parse & stringify json
 
-xhr.onload = function () {
-  //   console.log(xhr.response);
-  const listOfPosts = xhr.response;
+    xhr.onload = function () {
+      resolve(xhr.response); //this will be return in resolve
+      //   console.log(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const listOfPosts = responseData;
   //   console.log(listOfPosts);
+
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true); //this is for cloning/copy postTemplate
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
