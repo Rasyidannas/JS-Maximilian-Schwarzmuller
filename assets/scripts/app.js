1b8608539c00ab0be6fad6e5f8a5cbeb072b8b51
rasyid annas <rasshit.dsgn@gmail.com>
2024-01-13 16:03:14 +0700

    20/396. Splitting Code in a Sub-optimal Way.mp4

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,63 +1,27 @@
-const listElement = document.querySelector(".posts");
-const postTemplate = document.getElementById("single-post");
-const form = document.querySelector("#new-post form");
-const fetchButton = document.querySelector("#available-posts button");
-const postList = document.querySelector("ul");
-
-//this is function for get datas
-async function fetchPosts() {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
     );
 
-    // console.log(response);
-    const listOfPosts = response.data;
-    //   console.log(listOfPosts);
+    // const timerId = setTimeout(this.startAnalytics, 3000);
 
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true); //this is for cloning/copy postTemplate
-      postEl.querySelector("h2").textContent = post.title.toUpperCase();
-      postEl.querySelector("p").textContent = post.body;
-      postEl.querySelector("li").id = post.id;
-      listElement.append(postEl);
-    }
-  } catch (error) {
-    alert(error.message);
-    console.log(error);
+    // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+    //   clearTimeout(timerId);
+    // });
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement("script");
+    analyticsScript.src = "assets/scripts/analytics.js";
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
   }
 }
 
-//this is for upload data
-async function createPost(title, content) {
-  const userId = Math.random();
-  const post = {
-    title: title,
-    body: content,
-    userId: userId,
-  };
-
-  const response = await axios.post(
-    "https://jsonplaceholder.typicode.com/posts",
-    post
-  );
-  console.log(response);
-}
-
-fetchButton.addEventListener("click", fetchPosts);
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector("#title").value;
-  const enteredContent = event.currentTarget.querySelector("#content").value;
-
-  createPost(enteredTitle, enteredContent);
-});
-
-postList.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
-    const postId = event.target.closest("li").id;
-    //  console.log(postId);
-    axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-  }
-});
+App.init();
